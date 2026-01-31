debd5062d0242de6a477899188967035231b6ef7
Ubuntu <ubuntu@ip-172-31-92-77.ec2.internal>
2026-01-31 08:14:54 +0000

    leaks corrected

--- src/main/java/hello/SecretController.java
+++ src/main/java/hello/SecretController.java
@@ -1,5 +1,10 @@
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class SecretController {
-    // Hardcoded API Token
-    private String githubToken = "ghp_nS1yOEcfP5pvfqJml36mF7AkyHsEU0IU36mF";
+
+     
+    private final String githubToken = System.getenv("GITHUB_TOKEN");
+
 }
+
